183af18f7c3024a5de46f18dd90ab1bb0a54009c
Edgar Manuel García González <manolo161070@hotmail.com>
2014-12-11 12:19:22 -0600

    Inicio de Colas, estaLlena y estaVacia

--- Cola_Banco/src/cola_banco/Cola_Banco.java
+++ Cola_Banco/src/cola_banco/Cola_Banco.java
@@ -8,4 +8,26 @@ public class Cola_Banco
     public String[]COLA;
     public String[] COLA2;
     
+    public Cola_Banco(int tamaño) 
+                {
+                    
+                    this.tamaño = tamaño;
+                    frente = ultimo = -1;
+                    COLA = new String[tamaño];
+                    COLA2 = new String[tamaño];
+                }
+                public boolean estaLlena()
+                {
+                    if (ultimo>=tamaño -1)
+                        return true;
+                    else return false;
+                }
+
+                public boolean estaVacia()
+                {
+                    if (frente==-1)
+                        return true;
+                    else return false;
+                }
+    
 }
